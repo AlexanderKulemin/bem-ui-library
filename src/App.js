c723c6f988a548c0e074d10eba78c7545bb52366
alex.kulemin <alex.kulemin@sharp-dev.net>
2023-03-03 15:14:10 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
-import React, {useEffect, useState} from 'react';
-import './App.scss';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Aside from './template/components/Aside';
-import Header from './template/components/Header';
+import Aside from "./template/components/Aside";
+import Header from "./template/components/Header";
 
-import {HomePage} from './template/pages/HomePage';
-import {CheckboxPage} from './template/pages/CheckboxPage';
-import {ButtonsPage} from './template/pages/ButtonsPage';
-import {RadioPage} from './template/pages/RadioPage';
-import {ModalPage} from './template/pages/ModalPage';
-import {InputsPage} from './template/pages/InputsPages';
-import TextAreaPage from './template/pages/TextAreaPage';
-import {IconsPage} from './template/pages/IconsPage';
-import {TabsPage} from './template/pages/TabsPage';
-import {AutoCompletePage} from './template/pages/AutoCompletePage';
-import {SwitchPage} from './template/pages/SwitchPage';
-import StepsPage from './template/pages/StepsPage';
-import SelectPage from './template/pages/SelectPage';
-import PaginationPage from './template/pages/PaginationPage';
-import SliderPage from './template/pages/SliderPage';
-import ThemeContext from './template/components/ThemeContext';
+import { HomePage } from "./template/pages/HomePage";
+import { CheckboxPage } from "./template/pages/CheckboxPage";
+import { ButtonsPage } from "./template/pages/ButtonsPage";
+import { RadioPage } from "./template/pages/RadioPage";
+import { ModalPage } from "./template/pages/ModalPage";
+import { InputsPage } from "./template/pages/InputsPages";
+import TextAreaPage from "./template/pages/TextAreaPage";
+import { IconsPage } from "./template/pages/IconsPage";
+import { TabsPage } from "./template/pages/TabsPage";
+import { AutoCompletePage } from "./template/pages/AutoCompletePage";
+import { SwitchPage } from "./template/pages/SwitchPage";
+import StepsPage from "./template/pages/StepsPage";
+import SelectPage from "./template/pages/SelectPage";
+import PaginationPage from "./template/pages/PaginationPage";
+import SliderPage from "./template/pages/SliderPage";
+import ThemeContext from "./template/components/ThemeContext";
+
+const basename =
+  process.env.NODE_ENV === "dev" ? process.env.REACT_APP_BUILD_URL : "/";
 
 function App() {
-
-  const defaultTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'barnas'
+  const defaultTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : "barnas";
   const [theme, setTheme] = useState(defaultTheme);
 
   useEffect(() => {
     setTheme(theme);
-    localStorage.setItem('theme', theme)
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   return (
-      <Router>
-        <ThemeContext.Provider value={theme}>
-          <div className="app__box">
-            <div className="app__mask"></div>
-            <Aside/>
-            <div className="app">
-              <Header setTheme={setTheme} current={theme}/>
-              <main className="app__content">
-                <div className="content__wrp content__wrp_wh">
-                  <div className="content">
-                    <Switch>
-                      <Route path="/" exact component={HomePage}/>
-                      <Route path="/buttons" component={ButtonsPage}/>
-                      <Route path="/checkbox" component={CheckboxPage}/>
-                      <Route path="/radio" component={RadioPage}/>
-                      <Route path="/modal" component={ModalPage}/>
-                      <Route path="/inputs" component={InputsPage}/>
-                      <Route path="/icons" component={IconsPage}/>
-                      <Route path="/tabs" component={TabsPage}/>
-                      <Route path="/autocomplete" component={AutoCompletePage}/>
-                      <Route path="/switch" component={SwitchPage}/>
-                      <Route path="/steps" component={StepsPage}/>
-                      <Route path="/select" component={SelectPage}/>
-                      <Route path="/pagination" component={PaginationPage}/>
-                      <Route path="/textarea" component={TextAreaPage}/>
-                      <Route path="/slider" component={SliderPage}/>
-                    </Switch>
-                  </div>
+    <Router basename={basename}>
+      <ThemeContext.Provider value={theme}>
+        <div className="app__box">
+          <div className="app__mask"></div>
+          <Aside />
+          <div className="app">
+            <Header setTheme={setTheme} current={theme} />
+            <main className="app__content">
+              <div className="content__wrp content__wrp_wh">
+                <div className="content">
+                  <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/buttons" component={ButtonsPage} />
+                    <Route path="/checkbox" component={CheckboxPage} />
+                    <Route path="/radio" component={RadioPage} />
+                    <Route path="/modal" component={ModalPage} />
+                    <Route path="/inputs" component={InputsPage} />
+                    <Route path="/icons" component={IconsPage} />
+                    <Route path="/tabs" component={TabsPage} />
+                    <Route path="/autocomplete" component={AutoCompletePage} />
+                    <Route path="/switch" component={SwitchPage} />
+                    <Route path="/steps" component={StepsPage} />
+                    <Route path="/select" component={SelectPage} />
+                    <Route path="/pagination" component={PaginationPage} />
+                    <Route path="/textarea" component={TextAreaPage} />
+                    <Route path="/slider" component={SliderPage} />
+                  </Switch>
                 </div>
-              </main>
-            </div>
+              </div>
+            </main>
           </div>
-        </ThemeContext.Provider>
-      </Router>
+        </div>
+      </ThemeContext.Provider>
+    </Router>
   );
 }
 
